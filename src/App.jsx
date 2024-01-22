506e28f640e149c7b839d4fb0a5b476546d76271
Katie Vargas <you@example.com>
2024-01-21 22:21:13 -0500

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import ContactList from './ContactList';
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
-const App = () => {
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList />
+      )}
     </>
   );
 }
-
-export default App;
